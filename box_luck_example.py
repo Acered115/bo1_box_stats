efc9e9182a7d3f7fcc78aae3049404443d04443a
Acered <acered115@yahoo.com>
2024-04-07 02:02:27 +0100

    Added a DivideByZero catch for low box hit numbers, added a ValueError for the gun in the list, also added a change of poly degree for num_run values above 1000

--- box_luck_example.py
+++ box_luck_example.py
@@ -1,101 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-
-
-gun_list = [
-    "tgun",
-    "gersch",
-    "dolls",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-]
-
-
-def run_box_hits(num_box_hits: int):
-    current_hit = 0
-    count_dict = {}
-    for x in gun_list:
-        count_dict[x] = 0
-
-    while current_hit < num_box_hits:
-        current_hit += 1
-        randint = np.random.randint(0, len(gun_list))
-        curr_gun = gun_list[randint]
-        count_dict[curr_gun] += 1
-
-    return count_dict
-
-
-def create_list_from_dict(count_dict: dict, sort: bool = True) -> list[dict]:
-    _list = []
-    for gun_name in gun_list:
-        _list.append([gun_name, count_dict[gun_name]])
-
-    if sort == True:
-        return sorted(_list, key=lambda x: x[1])
-    else:
-        return _list
+from box_calc_utils import run_box_hits, create_list_from_dict, default_gun_list
 
 
 def create_plot(
     target_gun: str,
     num_box_hits: int,
     num_runs: int,
-    incr_box_hits=0,
-    sort_list: bool = False,
+    gun_list: list[str] = default_gun_list,
+    incr_box_hits: int = 0,
 ):
+    if target_gun not in default_gun_list:
+        raise ValueError(
+            f"'{target_gun}' not found in the gun_list: {default_gun_list}"
+        )
     t1 = time()
 
     target_gun_array = []
     least_common_array = []
     most_common_array = []
-    test = 0
+    special_gun_counter = 0
+
     for step in np.arange(0, num_runs):
         num_box_hits += incr_box_hits
-        count_dict = run_box_hits(num_box_hits)
+        count_dict = run_box_hits(num_box_hits=num_box_hits, gun_list=gun_list)
 
         min_gun = min(count_dict, key=lambda k: count_dict[k])
         max_gun = max(count_dict, key=lambda k: count_dict[k])
         least_common = [min_gun, count_dict[min_gun]]
         most_common = [max_gun, count_dict[max_gun]]
 
-        target_gun_array.append(num_box_hits / count_dict[target_gun])
-        least_common_array.append(num_box_hits / least_common[1])
-        most_common_array.append(num_box_hits / most_common[1])
-        # counted_list = create_list_from_dict(count_dict)
-        # print(counted_list)
-        # for index, _ in enumerate(counted_list):
-        #     if counted_list[index][0] == target_gun:
-        #         avg_array.append(num_box_hits / counted_list[index][1])
-        #         break
-        # print(least_common)
-        # print(type(count_dict[target_gun]))
+        try:
+            target_gun_array.append(num_box_hits / count_dict[target_gun])
+        except ZeroDivisionError:
+            target_gun_array.append(0)
+        try:
+            least_common_array.append(num_box_hits / least_common[1])
+        except ZeroDivisionError:
+            least_common_array.append(0)
+
+        try:
+            most_common_array.append(num_box_hits / most_common[1])
+        except ZeroDivisionError:
+            most_common_array.append(0)
+
         if (
             most_common[1] == count_dict[target_gun]
-            and num_box_hits / count_dict[target_gun] < 16.5
+            and num_box_hits / count_dict[target_gun] < 16.5524475
         ):
-            test += 1
+            special_gun_counter += 1
             print(
-                f"Game {step}, {target_gun} was most common,{test} times, with a trade avg of {num_box_hits/count_dict[target_gun]} out of {num_box_hits} box hits"
+                f"Game {step}, {target_gun} was most common, {special_gun_counter} times, with a trade avg of {round(num_box_hits/count_dict[target_gun],2)} out of {num_box_hits} box hits,\nCurrent ratio: 1 in {round(step/special_gun_counter,2)}."
             )
 
-        print(step, "\r", end="")
+        print(f"Current Game: {step}", "\r", end="")
 
     x_values = range(0, len(target_gun_array))
 
@@ -116,13 +75,19 @@ def create_plot(
         x_values, least_common_array, color="blue", label="Highest Trade Ratio", s=1
     )
 
+    if num_runs < 1000:
+        poly_degree = 2
+    else:
+        poly_degree = 3
+
     # Fit a line of best fit (polynomial of degree 4) to the "Trade Ratio" data
-    z_target = np.polyfit(x_values, target_gun_array, 2)
+    z_target = np.polyfit(x_values, target_gun_array, poly_degree)
     p_target = np.poly1d(z_target)
-    z_most_comm = np.polyfit(x_values, most_common_array, 2)
+    z_most_comm = np.polyfit(x_values, most_common_array, poly_degree)
     p_most_comm = np.poly1d(z_most_comm)
-    z_least_comm = np.polyfit(x_values, least_common_array, 2)
+    z_least_comm = np.polyfit(x_values, least_common_array, poly_degree)
     p_least_comm = np.poly1d(z_least_comm)
+
     plt.plot(
         x_values,
         p_target(x_values),
@@ -150,7 +115,7 @@ def create_plot(
     )
     # Plot the line of best fit for the min values
 
-    plt.xlabel("Step")
+    plt.xlabel("Steps")
     plt.ylabel(f"Frequency of selected gun")
     plt.title(f"Trade Ratio of '{target_gun}' vs. Step")
     plt.grid(True)
@@ -160,7 +125,7 @@ def create_plot(
 
 
 if __name__ == "__main__":
-    num_box_hits = 250
-    number_of_games = 1000
+    num_box_hits = 236
+    number_of_games = 7000
 
-    create_plot("tgun", num_box_hits, number_of_games, incr_box_hits=10, sort_list=True)
+    create_plot("tgun", num_box_hits, number_of_games, incr_box_hits=1)
